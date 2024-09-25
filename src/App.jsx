a2618633ddc78189dbd4a4d77b67e39066b16995
Eliott Goninet <eliott.goninet@etu.univ-poitiers.fr>
2024-09-25 18:24:19 +0200

    jsp

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+import 
+function App() {
+  return (
+    <>
+    <h1>Marvel Characters</h1>
+      <ul id="characters">
+        <li>
+          Beast
+        </li>
+        <li>
+          Captain America
+        </li>
+        <li>
+          Deadpool
+        </li>
+      </ul>
+    </>
+  )
+}
+export default App
